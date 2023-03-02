57e795bc1130d10266f4da94d354f48cb5ef63e5
lordmoses <fxmoses727@gmail.com>
2023-03-02 13:07:58 -0500

    Update run_procmon.ps1

--- run_procmon.ps1
+++ run_procmon.ps1
@@ -1,5 +1,5 @@
 #param ($configfile, $backingfile)
-#C:\Users\tingwei\Desktop\ProcessMonitor\Procmon.exe /Quiet /LoadConfig $configfile /BackingFile $backingfile
+#C:\Users\username\Desktop\ProcessMonitor\Procmon.exe /Quiet /LoadConfig $configfile /BackingFile $backingfile
 
 param ($procmonlocation, $configfile, $backingfile)
 &$procmonlocation /LoadConfig $configfile /BackingFile $backingfile /Quiet /Minimized /AcceptEula
